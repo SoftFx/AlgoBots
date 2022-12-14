50102c6b3ef2dc2338afeab7aea42b76991c4c4c
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2022-12-14 17:42:33 +0300

    Portfolio bot: Precision for Equity has been added

--- 100YearPortfolio/Portfolio/PortfolioConfig.cs
+++ 100YearPortfolio/Portfolio/PortfolioConfig.cs
@@ -41,7 +41,7 @@ namespace _100YearPortfolio.Portfolio
                 sb.AppendLine($"{nameof(PortfolioConfig)}:")
                   .AppendLine($"{UpdateMinSettingName} = {UpdateMinutes}")
                   .AppendLine($"{BalanceTypeSettingName} = {BalanceType}")
-                  .AppendLine($"{EquityMinLevelSettingName} = {EquityMinLevel * 100.0}%")
+                  .AppendLine($"{EquityMinLevelSettingName} = {EquityMinLevel * 100.0:F4}%")
                   .AppendLine($"{EquityUpdateTimeName} = {EquityUpdateTime}")
                   .AppendLine()
                   .AppendLine("Optional settings:")
