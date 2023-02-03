c5628c146426f6f938ccf54e20972dac63666d44
Andrei Khloptsau <andrei.khloptsau@soft-fx.lv>
2023-02-03 19:49:40 +0300

    Try get correct path

--- .github/build/merge_metadata_script.ps1
+++ .github/build/merge_metadata_script.ps1
@@ -1,19 +1,24 @@
 param ([Parameter(Mandatory)][String]$sourceDir,
 	   [String]$resultFileName='metadata')
 
-$allFiles = @()
-$resultPath = "$sourceDir\$resultFileName.json"
-
-ForEach($file in Get-ChildItem $sourceDir -Filter *.json){
-	$fileName = $file.Name
-	Write-Host "New json file detected: $fileName"
-
-	$data = Get-Content -Path $sourceDir\$fileName -Raw | ConvertFrom-Json
-	$allFiles += $data
+if(!(Test-Path $sourceDir)) {
+	Write-Host "ERROR: Path not found.Check the path and try again!" -ForegroundColor Red
 }
+else {
+	$allFiles = @()
+    $resultPath = "$sourceDir\$resultFileName.json"
 
-Write-Host "Result file will be located in $resultPath"
+	ForEach($file in Get-ChildItem $sourceDir -Filter *.json){
+        $fileName = $file.Name
+        Write-Host "New json file detected: $fileName"
 
-$allFiles | ConvertTo-Json -Depth 5 -compress | Out-File -FilePath $resultPath
+		$data = Get-Content -Path $sourceDir\$fileName -Raw | ConvertFrom-Json
+		$allFiles += $data
+	}
 
-Write-Host "Result has been stored to $resultPath " -ForegroundColor Green
+    Write-Host "Result file will be located in $resultPath"
+
+	$allFiles | ConvertTo-Json -Depth 5 -compress | Out-File -FilePath $resultPath
+
+    Write-Host "Result has been stored to $resultPath " -ForegroundColor Green
+}
--- .github/build/rename_packages.ps1
+++ .github/build/rename_packages.ps1
@@ -2,25 +2,37 @@ param ([Parameter(Mandatory)][String]$sourceDir,
 	   [Parameter(Mandatory)][String]$releaseVersion,
 	   [String]$isPreRelese="false")
 
-ForEach($file in Get-ChildItem $sourceDir -Filter *.ttalgo){
-	$filePath = $file.FullName
+Write-Host "Raw sourceDir = $sourceDir"
 
-	Write-Host "Algo package detected: $filePath"
+# $sourceDir = $sourceDir -replace '[\\]', '/'
 
-	$directory = $file.DirectoryName
-	$baseName = $file.Basename
-	if ($isPreRelese.ToLower() -eq "true"){
-		$prefix = "preRelease"
-	}
-	else {
-		$prefix="release"
-	}
+# Write-Host "Fixed sourceDir = $sourceDir"
 
-	Rename-Item -Path $filePath -NewName "$directory/$baseName($prefix-v.$releaseVersion).ttalgo"
+if(!(Test-Path $sourceDir)) {
+	Write-Host "ERROR: Path $sourceDir not found. Check the path and try again!" -ForegroundColor Red
 }
+else
+{
+	ForEach($file in Get-ChildItem $sourceDir -Filter *.ttalgo){
+        $filePath = $file.FullName
 
-Write-Host "New package names:"
+        Write-Host "Algo package detected: $filePath"
 
-ForEach($file in Get-ChildItem $sourceDir -Filter *.ttalgo){
-	Write-Host $file -ForegroundColor Green
+		$directory = $file.DirectoryName
+		$baseName = $file.Basename
+		if ($isPreRelese.ToLower() -eq "true"){
+			$prefix = "preRelease"
+		}
+		else {
+			$prefix="release"
+		}
+
+		Rename-Item -Path $filePath -NewName "$directory/$baseName($prefix-v.$releaseVersion).ttalgo"
+	}
+
+    Write-Host "New package names:"
+
+	ForEach($file in Get-ChildItem $sourceDir -Filter *.ttalgo){
+		Write-Host $file -ForegroundColor Green
+	}
 }
